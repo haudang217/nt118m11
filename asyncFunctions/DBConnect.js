4d15f8e52cbe39d4334eb4456bb744fd12116718
nduytung <nduytung.1611@gmail.com>
2021-12-13 09:50:31 +0700

    final

--- asyncFunctions/DBConnect.js
+++ asyncFunctions/DBConnect.js
@@ -4,8 +4,8 @@ require("dotenv").config();
 const DBConnect = async () => {
   try {
     await mongoose.connect(
-      `mongodb+srv://${process.env.DB_USER_NAME}:${process.env.DB_PASSWORD}@db01.99ijd.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`,
-      //"mongodb://127.0.0.1:27017",
+      //`mongodb+srv://${process.env.DB_USER_NAME}:${process.env.DB_PASSWORD}@db01.99ijd.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`,
+      "mongodb://127.0.0.1:27017",
 
       {
         useNewUrlParser: true,
